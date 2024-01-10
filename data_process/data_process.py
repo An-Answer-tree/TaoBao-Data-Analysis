1868c803e80c3c77f1036780e08289cca00d3ad8
An-Answer-tree <szliutong@qq.com>
2024-01-10 17:20:29 +0800

    将代码导入jupyter notebook

--- data_process/data_process.py
+++ data_process/data_process.py
@@ -9,6 +9,7 @@ print('最初数据类型：\n', df.dtypes, '\n\n')
 # 数据预处理
 # 在df中去除有空值的数据项，并在原地修改
 df.dropna(axis=0, how='any', inplace=True)
+print('去除空值后数据为：\n', df, '\n\n')
 
 # 在df中去除所有字段相同的重复值，保留第一个
 # print(df.drop_duplicates(ignore_index=True))
@@ -17,6 +18,6 @@ df.dropna(axis=0, how='any', inplace=True)
 df['mon'] = df['time'].str.split(pat='-', expand=True)[1]
 df['day'] = df['time'].str.split(pat='-', expand=True)[2]
 df['hour'] = df['time'].str.split(pat=' ', expand=True)[1]
-df.drop(labels='time', axis=1, inplace=True)
+# 修改mon，day，hour字段的数据类型
 print('当前数据：\n', df.head(), '\n\n')
 print('当前df数据类型：\n', df.dtypes, '\n\n')
